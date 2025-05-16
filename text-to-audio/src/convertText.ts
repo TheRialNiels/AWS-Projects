69eb5be4e1681b43682355208db1fe5a22f81cc3
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-05-15 21:06:11 -0600

    Rename file

--- text-to-audio/src/convertText.ts
+++ text-to-audio/src/convertText.ts
@@ -11,20 +11,20 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  */
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'retrieve text',
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'some error happened',
+      }),
+    };
+  }
 };
