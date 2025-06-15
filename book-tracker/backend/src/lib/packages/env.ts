fe3f4029070c39738f3e989fd2d0af2d21015744
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-06-14 19:22:24 -0600

    Add CORS support and utility functions, implement UUID generation, and enhance environment variable management

---
+++ book-tracker/backend/src/lib/packages/env.ts
@@ -0,0 +1,11 @@
+import { get } from 'env-var'
+
+export const env = {
+  ALLOW_ORIGIN: get('ALLOW_ORIGIN').required().asString(),
+  ALLOW_CREDENTIALS: get('ALLOW_CREDENTIALS').required().asString(),
+  REGION: get('REGION').required().asString(),
+  ENVIRONMENT: get('ENVIRONMENT').required().asString(),
+  BOOKS_TABLE: get('BOOKS_TABLE').required().asString(),
+  BOOKS_TITLE_GSI: get('BOOKS_TITLE_GSI').required().asString(),
+  BOOKS_AUTHOR_GSI: get('BOOKS_AUTHOR_GSI').required().asString(),
+}
