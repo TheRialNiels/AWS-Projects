313a4ad3979990e1e3f6b6bb4ebc483a0d18a164
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-06-17 21:39:50 -0600

    Add task management features with schema, data generation, and table components

---
+++ table-component/components/table/data-table-toolbar.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { DataTableFacetedFilter } from './data-table-faceted-filter'
+import { DataTableViewOptions } from './data-table-view-options'
+import { Input } from '@/components/ui/input'
+import type { Table } from '@tanstack/react-table'
+import { X } from 'lucide-react'
+import { cn } from '@/lib/utils'
+
+interface DataTableToolbarProps<TData> {
+  table: Table<TData>
+  filterColumn: string
+  filterPlaceholder?: string
+  facetedFilters?: FacetedFilter[]
+  showViewBtn?: boolean
+  showAddBtn?: boolean
+  addBtnLabel?: string
+  onFilterChange?: (value: string) => void
+}
+
+export interface FacetedFilter {
+  key: string
+  title: string
+  options: FilterOptions[]
+}
+
+interface FilterOptions {
+  label: string
+  value: string
+  icon?: React.ComponentType<{ className?: string }>
+}
+
+export function DataTableToolbar<TData>({
+  table,
+  filterColumn,
+  filterPlaceholder = 'Search...',
+  facetedFilters,
+  showViewBtn = true,
+  showAddBtn = true,
+  addBtnLabel = 'Add',
+  onFilterChange,
+}: DataTableToolbarProps<TData>) {
+  const column = table.getColumn(filterColumn ?? '')
+  const filterValue = (column?.getFilterValue() as string) ?? ''
+  const isFiltered = table.getState().columnFilters.length > 0
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    column?.setFilterValue(value)
+    onFilterChange?.(value)
+  }
+
+  return (
+    <div className="flex gap-4 items-start justify-between">
+      <div className="flex flex-wrap flex-1 items-center gap-2">
+        <Input
+          placeholder={filterPlaceholder}
+          value={filterValue}
+          onChange={handleFilterChange}
+          className="h-8 w-full max-w-xs sm:max-w-[15rem]"
+        />
+        {facetedFilters?.map(
+          ({ key, title, options }) =>
+            table.getColumn(key) && (
+              <DataTableFacetedFilter
+                key={key}
+                column={table.getColumn(key)!}
+                title={title}
+                options={options}
+              />
+            ),
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => table.resetColumnFilters()}
+          >
+            Reset
+            <X />
+          </Button>
+        )}
+      </div>
+      {(showViewBtn || showAddBtn) && (
+        <div
+          className={cn(
+            'flex items-center gap-2',
+            showViewBtn && !showAddBtn && 'hidden md:flex',
+          )}
+        >
+          {showViewBtn && <DataTableViewOptions table={table} />}
+          {showAddBtn && (
+            <Button size="sm" className="h-8">
+              {addBtnLabel}
+            </Button>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ table-component/components/tasks/tasks-table.tsx
@@ -0,0 +1,65 @@
+import { priorities, statuses } from '@/data/data'
+
+import { DataTable } from '@/components/table/data-table'
+import type { FacetedFilter } from '@/components/table/data-table-toolbar'
+import type { Task } from '@/data/schema'
+import {
+    getTaskColumns,
+    type GetTaskColumnsProps,
+} from '@/components/table/columns'
+
+interface TasksTableProps<TData extends Task> {
+    data: TData[]
+    isLoading: boolean
+    isError: boolean
+}
+
+export function TasksTable<TData extends Task>({
+    data,
+    isLoading,
+    isError,
+}: TasksTableProps<TData>) {
+    const onEdit = (row: Task) => {
+        console.log('🚀 ~ onEdit ~ row:', row)
+    }
+    const onDelete = (id: string) => {
+        console.log('🚀 ~ onDelete ~ id:', id)
+    }
+
+    const showViewBtn = false
+    const columnsConfig: GetTaskColumnsProps = {
+        showViewBtn,
+        rowActionsStyle: 'row',
+        onEdit,
+        onDelete,
+    }
+    const columns = getTaskColumns(columnsConfig)
+    const facetedFilters: FacetedFilter[] = [
+        {
+            key: 'status',
+            title: 'Status',
+            options: statuses,
+        },
+        {
+            key: 'priority',
+            title: 'Priority',
+            options: priorities,
+        },
+    ]
+
+    return (
+        <DataTable
+            columns={columns}
+            data={data}
+            pageSize={10}
+            isLoading={isLoading}
+            isError={isError}
+            filterColumn="title"
+            filterPlaceholder="Search by title..."
+            facetedFilters={facetedFilters}
+            showViewBtn={showViewBtn}
+            addBtnLabel="Add task"
+            rowsPerPage={[10, 20, 25]}
+        />
+    )
+}
