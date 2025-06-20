30ccd78a529f51dc56f54b3e6ac6a24f7368b61c
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-06-19 22:09:09 -0600

    Refactor useCreateBook to support optimistic updates with temporary ID generation and customizable success/error messages

--- book-tracker/frontend/src/services/mutations/books.mutations.ts
+++ book-tracker/frontend/src/services/mutations/books.mutations.ts
@@ -6,18 +6,33 @@ import {
 } from '@/services/api/books.api'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
+import { generateUuid } from '@/lib/uuid'
 import { useOptimisticMutation } from '@/services/mutations/useOptimistic.mutations'
 
-export const useCreateBook = () => {
-  const queryClient = useQueryClient()
-
-  return useMutation({
-    mutationFn: (data: Book) => postBookApi(data),
-
-    onSettled: async (_, error) => {
-      if (error) return
-      await queryClient.invalidateQueries({ queryKey: ['getBooks'] })
+export const useCreateBook = (
+  setOpen: (open: boolean) => void,
+  successMsg: string,
+  errorMsg: string,
+) => {
+  return useOptimisticMutation<Book, Book>({
+    queryKey: ['getBooks'],
+    getId: (book) => book.id!,
+    mutationFn: postBookApi,
+    getItems: (data: GetBooksResponse) => data.responseData.books,
+    setItems: (newBooks, oldData: GetBooksResponse) => ({
+      ...oldData,
+      responseData: {
+        ...oldData.responseData,
+        books: newBooks,
+      },
+    }),
+    updateItems: (prevBooks, newBook) => {
+      if (!newBook.id) newBook.id = generateUuid() // * Temp ID
+      return [...prevBooks, newBook]
     },
+    successMsg,
+    errorMsg,
+    onDone: () => setOpen(false),
   })
 }
 
