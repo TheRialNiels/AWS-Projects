046d7a0395183815d3e1fb0288c5a38583009841
Jonathan Dominguez <jonathan24.dp@gmail.com>
2025-06-21 20:37:30 -0600

    Add Tooltip component and integrate it into DataTableRowActions for enhanced user interaction

--- book-tracker/frontend/src/components/table/data-table-row-actions.tsx
+++ book-tracker/frontend/src/components/table/data-table-row-actions.tsx
@@ -3,6 +3,11 @@
 import { SquarePen, Trash2 } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { BookSchema, type Book } from '@/interfaces/books.types'
 import type { Row } from '@tanstack/react-table'
 
@@ -25,26 +30,39 @@ export function DataTableRowActions<TData>({
 
   return (
     <div className="inline-flex gap-3">
-      <Button
-        className="size-8"
-        variant="secondary"
-        size="icon"
-        title={editLabelBtn}
-        onClick={() => onEdit(task)}
-      >
-        <SquarePen />
-        <span className="sr-only">{editLabelBtn}</span>
-      </Button>
-      <Button
-        className="size-8"
-        variant="destructive"
-        size="icon"
-        title={deleteLabelBtn}
-        onClick={() => onDelete(task)}
-      >
-        <Trash2 />
-        <span className="sr-only">{deleteLabelBtn}</span>
-      </Button>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="size-8"
+            variant="secondary"
+            size="icon"
+            onClick={() => onEdit(task)}
+          >
+            <SquarePen />
+            <span className="sr-only">{editLabelBtn}</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{editLabelBtn}</p>
+        </TooltipContent>
+      </Tooltip>
+
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="size-8"
+            variant="destructive"
+            size="icon"
+            onClick={() => onDelete(task)}
+          >
+            <Trash2 />
+            <span className="sr-only">{deleteLabelBtn}</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="bg-destructive fill-destructive text-destructive-foreground">
+          <p>{deleteLabelBtn}</p>
+        </TooltipContent>
+      </Tooltip>
     </div>
   )
 }
